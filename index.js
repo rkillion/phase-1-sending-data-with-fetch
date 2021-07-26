bbb0307e0746c7bdcf13038de1bd6fcd67128cd7
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-07-26 18:01:59 -0400

    commit tree d9cc4b3ef7dcb3ccd75f783f9880697df4b3cafc

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const formData = {
+    dogName: "Sammy",
+    dogBreed: "Poodle"
+  };
+  
+  function configurationObject(data) {
+      return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+   }
+};
+
+function submitData(userName,userEmail) {
+    return fetch("http://localhost:3000/users", configurationObject({name: userName,email: userEmail}))
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+        document.body.append(object.id);
+    })
+    .catch(function(error) {
+        document.body.append(error.message);
+    });
+}
